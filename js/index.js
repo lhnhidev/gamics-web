7a12a2ae0954ace07c7ad260a87d22ef676111f8
Nhi-Le <lhnhi420@gmail.com>
2024-09-13 23:30:42 +0700

    fit index.js slogan

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 const sloganTextOne = 'Chiến thuật đỉnh cao, nhân vật định hình chiến thắng.';
 const sloganTextTwo = 'Chiến đấu cùng đồng đội, chiến thắng bằng chiến thuật.';
 
+var slogan = sloganTextOne;
 var index = 0;
 
 function renderSlogan() {
-    if (index < sloganTextOne.length) {
-        document.querySelector('#slogan').innerHTML += sloganTextOne[index];
+    if (index < slogan.length) {
+        document.querySelector('#slogan').innerHTML += slogan[index];
         index++;
         setTimeout(renderSlogan, 50);
     }
