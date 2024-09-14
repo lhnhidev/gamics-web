3c70decbb125bebe2f917e78bff755d149f7db33
Nhi-Le <lhnhi420@gmail.com>
2024-09-14 14:45:02 +0700

    create search visual in header HTML CSS

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
-const sloganTextOne = 'Chiến thuật đỉnh cao, nhân vật định hình chiến thắng.';
-const sloganTextTwo = 'Chiến đấu cùng đồng đội, chiến thắng bằng chiến thuật.';
+    /*
+        ! Render slogan in Banner
+    */ 
+
+const sloganTextOne = 'Chiến thuật đỉnh cao, nhân vật định hình chiến thắng';
+const sloganTextTwo = 'Chiến đấu cùng đồng đội, chiến thắng bằng chiến thuật';
 
 var slogan = sloganTextOne;
 var index = 0;
@@ -13,3 +17,21 @@ function renderSlogan() {
 }
 
 renderSlogan();
+
+    /*
+        ! Show and Hidden serach input when click search button in Header
+    */
+
+const searchBtn = document.querySelector('#searchBtn');
+searchBtn.addEventListener('click', () => {
+    document.querySelector('.find').classList.remove('hidden');
+    document.querySelector('.find').style.animation = 'fadeIn ease-in-out .5s forwards';
+});
+
+const findBackground = document.querySelector('#findBackground');
+findBackground.addEventListener('click', () => {
+    document.querySelector('.find').style.animation = 'fadeOut ease-in-out .5s forwards';
+    setTimeout(() => {
+        document.querySelector('.find').classList.add('hidden');
+    }, 500);
+});
